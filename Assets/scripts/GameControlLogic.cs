455c244474ff7b348939cefaca05184e20efa624
SashaSenichkin <30441534+SashaSenichkin@users.noreply.github.com>
2017-07-25 19:32:18 +0300

    initial commit

---
+++ Assets/scripts/GameControlLogic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlLogic : MonoBehaviour {
+
+    public GameObject[] AsteroidHazard1;
+    public int ZValue;
+    public int XSize;
+    public float SpawnSpeed;
+    public UnityEngine.UI.Text ScoreText;
+    public UnityEngine.UI.Text GameOverText;
+    private int fScore = 50;
+    private bool GameOver = false;
+
+    /*
+    снимать очки за каждый выстрел
+    добавлять за каждый сбитый корабль и за каждый астероид, уничтоженный границей
+    возможные улучшения: добавление небольшого количества очков за уничтоженные астероиды
+    ускорение перезарядки
+    двойной-тройной выстрел
+    укрепление брони игрока
+    увеличение наград, уменьшение цен
+    установка мин
+    притягивающий луч (кинул астероид в нужное место - больше награда)
+    */
+
+
+    public int Score
+    {
+        get { return fScore; }
+        set {
+            if (fScore == value)
+                return;
+            fScore = value;
+            UpdateScore();
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        while (!GameOver)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-XSize, XSize), 0, ZValue);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(AsteroidHazard1[Random.Range(0, 3)], spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(SpawnSpeed);
+        }
+        yield break;
+    }
+    // Use this for initialization
+    private void Start () {
+       GameOverText.enabled = false;
+       StartCoroutine( SpawnWaves());
+       Score = 0;
+       UpdateScore();
+    }
+    private void UpdateScore()
+    {
+        ScoreText.text = "Score: " + Score;
+    }
+    public void SetTextGameOver()
+    {
+        GameOverText.enabled = true;
+        GameOver = true;
+    }
+}
---
+++ Assets/scripts/onDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onDestroy : MonoBehaviour {
+    public GameObject expolosion;
+    void OnDestroy()
+    {
+        if (this.gameObject.tag == "BoundDestroy")
+            return;
+        GameObject newAstExplose = Instantiate(expolosion, transform.position, transform.rotation);
+        Destroy(newAstExplose, 2);
+    }
+}
